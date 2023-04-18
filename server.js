1b710a6414fe87fea8dcf07278fa4410dc0f2544
Sam Jones <samejones2018@gmail.com>
2023-04-17 18:02:05 -0700

    created boilerplate server and made sure server ran with nodemon

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+
+
+
+
+
+
+app.listen(PORT, () => console.log(`Server started on http://localhost:${PORT}`));
