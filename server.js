a920ee1ae23a35e5543b9921d77c901b12449a95
Sam Jones <samejones2018@gmail.com>
2023-04-20 17:45:38 -0700

    fixed links in html to js and css, debugged in terminal

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const apiRoutes = require('./routes/apiRoutes');
-
+const htmlroutes = require('./routes/htmlRoutes');
 
 
 const PORT = process.env.PORT || 3001;
@@ -9,9 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(apiRoutes);
+app.use(htmlroutes);
 app.use(express.static('public'));
-
-
-
 
 app.listen(PORT, () => console.log(`Server started on http://localhost:${ PORT }`));
