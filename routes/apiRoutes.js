971ac12d19017d15d1d71ce4a85a7eddafd508f0
Sam Jones <samejones2018@gmail.com>
2023-04-20 17:04:57 -0700

    working on api routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,18 +3,25 @@ const router = express.Router();
 const fs = require('fs');
 const uuid = require('uuid');
 const path = require('path');
+const db = path.join(__dirname, '../db/db.json');
 
+const readNotes = () => {
+    const notes = fs.readFileSync(db, 'utf8');
+    return JSON.parse(notes);
+};
+
+const writeNotes = (notes) => {
+    fs.writeFileSync(db, JSON.stringify(notes));
+};
 
 //Get all notes
 router.get('/api/notes', (req, res) => {
-     
-
     });
 
 //Add new notes
 router.post('/api/notes', (req, res) => {
-    
-
+    const notes = readNotes();
+    res.json(notes);
     });
 
 //Delete notes
