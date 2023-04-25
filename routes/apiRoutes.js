5501f28dc411a1209b879d16136ef51367e4cd59
Sam Jones <samejones2018@gmail.com>
2023-04-24 19:34:28 -0700

    updated to try catch instead per ta rec

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -15,38 +15,42 @@ const writeNotes = (notes) => {
     fs.writeFileSync(db, JSON.stringify(notes));
 };
 
-//Get all notes
+// Get all notes
 router.get('/api/notes', (req, res) => {
-    const notes = readNotes();
-    if (err){
+    try {
+        const notes = readNotes();
+        res.json(notes);
+    } catch (err) {
         return res.status(500).json(err);
     }
-    res.json(notes);
-    });
+});
 
-//Add new notes
+// Add new notes
 router.post('/api/notes', (req, res) => {
-    const newNote = { ...req.body, id: uuid.v4() };
-    const notes = readNotes();
-    notes.push(newNote);
-    writeNotes(notes);
-    if(err){
+    try {
+        const newNote = { ...req.body, id: uuid.v4() };
+        const notes = readNotes();
+        notes.push(newNote);
+        writeNotes(notes);
+        res.json(notes);
+    } catch (err) {
         return res.status(500).json(err);
     }
-    res.json(notes);
-    });
+});
 
-//Delete notes
+// Delete notes
 router.delete('/api/notes/:id', (req, res) => {
-    const noteId = req.params.id;
-    const notes = readNotes();
-    const updateNotes = notes.filter((note) => note.id !== noteId);
-    writeNotes(updateNotes);
-    if(err){
+    try {
+        const noteId = req.params.id;
+        const notes = readNotes();
+        const updateNotes = notes.filter((note) => note.id !== noteId);
+        writeNotes(updateNotes);
+        res.json(updateNotes);
+    } catch (err) {
         return res.status(500).json(err);
     }
-    res.json(updateNotes);
-    });
+});
+
 
 
 module.exports = router;
